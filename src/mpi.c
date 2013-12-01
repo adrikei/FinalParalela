8ac7ed4f605d72f587a2f4a1fc01ebe3b6cda983
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-12-01 09:40:17 -0200

    adicionado MPI

--- src/mpi.c
+++ src/mpi.c
@@ -1,26 +1,117 @@
-/*
- * Grupo 13:
- * 329550-Adriano Pais Rodrigues
- * 380075-Arthur Pessoa de Souza
- * 379980-João Eduardo Brandes Luiz
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-int main(void) {
+#include <string.h>
+#include <mpi.h>
 
-   int thread_count;
+#define MAX 1000
+int vetor[MAX][MAX];
+int main(){
 
-   thread_count = 1;
-
-  time_t rawtime;
-  struct tm * timeinfo;
-
-  time ( &rawtime );
-  timeinfo = localtime ( &rawtime );
-
-   printf("%d-%s%d-%s%d-%s%d-%s", thread_count, asctime (timeinfo), thread_count, asctime (timeinfo), thread_count, asctime (timeinfo), thread_count, asctime (timeinfo));
-
-   return 0;
+	int comm_size;   //Numero de processos total
+	int my_rank;   // identificacao do processo
+	
+	int num_elem=0;
+	int i,j,resto;
+	int qtd_por_processo;
+	int flag=0,flag_g=0;
+	int limite=0;
+	MPI_Init(NULL,NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	do{
+		flag_g = 0;
+		flag = 0;
+		if(my_rank==0){
+			printf("\nEntre com o número de elementos: ");
+			scanf("%d",&num_elem);
+			if(num_elem == 0){
+				return 0;
+		}
+		}
+		
+		MPI_Bcast(&num_elem, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Barrier(MPI_COMM_WORLD);
+		if(my_rank==0){
+		printf("\nEntre com o vetor:\n");
+		for(i = 0; i < num_elem; i++){
+                        for(j = 0; j < num_elem; j++){
+                                scanf(" %d", &vetor[i][j]);
+                        }
+                }
+                }
+                MPI_Barrier(MPI_COMM_WORLD);
+               
+                
+		MPI_Bcast(vetor, MAX*MAX, MPI_INT, 0, MPI_COMM_WORLD);
+		
+		
+		
+		MPI_Barrier(MPI_COMM_WORLD);
+		if(my_rank==1){
+			for(i = 0; i < num_elem; i++){
+                        for(j = 0; j < num_elem; j++){
+                                printf("%d", vetor[i][j]);
+                        }
+                        printf("\n");
+                }
+		}
+		qtd_por_processo = (num_elem/comm_size);
+		resto = (num_elem%comm_size);
+		if(my_rank==0){
+			if(resto==0){
+			for(i=1;i<comm_size;i++){	
+				limite+=qtd_por_processo;
+				MPI_Send(&limite,1,MPI_INT,i,0,MPI_COMM_WORLD);
+			}
+		}else{
+//Se tiver resto, envia um elemento a mais para os "resto" primeiros processos
+			qtd_por_processo++;
+			for(i=1;i<resto;i++){
+				limite+=qtd_por_processo;
+				MPI_Send(&limite,1,MPI_INT,i,0,MPI_COMM_WORLD);
+			}
+			limite++;
+			qtd_por_processo--;
+			for(i=resto;i<comm_size;i++){	
+				limite+=qtd_por_processo;
+				MPI_Send(&limite,1,MPI_INT,i,0,MPI_COMM_WORLD);	
+			}
+			qtd_por_processo++;
+		}
+		limite=0;
+		}else{
+			MPI_Recv(&limite,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		}
+		
+		if(my_rank < resto){
+			qtd_por_processo++;
+		}
+		//printf("\nmy_rank: %d, limite: %d",my_rank,limite);
+		for(i=limite;i<limite+qtd_por_processo;i++){
+			for(j=0;j<num_elem;j++){
+				if(i!=j){
+					if(vetor[i][j]==0){
+					printf("\nFlag! %d %d %d",my_rank,i,j);
+					flag=1;
+					}
+				}
+			}
+		}
+		MPI_Reduce(&flag,&flag_g,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+		if(my_rank==0){
+			printf("\nFlag_g: %d",flag_g);
+			if(flag_g>=1){
+				printf("\nNot Complete!");
+			}else{
+				printf("\nComplete!");
+			}
+			
+		}
+		//printf("\nFlag: %d, Flag_g: %d",flag,flag_g);
+		//MPI_Bcast(&flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	}while(num_elem!=0);
+	MPI_Finalize();
+	
+      
+	return 0;
 }
